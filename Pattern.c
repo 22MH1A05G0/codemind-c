13ff269647d64b03a9734c0aae7d1e51f7f0f17c
22MH1A05G0 <121595363+22MH1A05G0@users.noreply.github.com>
2023-05-23 23:35:19 +0530

    #include<stdio.h> int main() {     int n;     scanf("%dx",&n);     for(int i=1;i<=n;i++){         for(int j=n;j>=1;j--){             printf("%d ",i);         }         printf(" ");     } }

---
+++ Pattern.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%dx",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=n;j>=1;j--){
+            printf("%d ",i);
+        }
+        printf("
+");
+    }
+}
